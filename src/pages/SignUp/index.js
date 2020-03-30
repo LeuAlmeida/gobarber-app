a5977b30d587c2c3025e464468f9dced3ef7fa82
Léu Almeida <leo@webid.net.br>
2020-03-30 17:06:11 -0300

    feat(pages/SignUp): improving structure

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text } from 'react-native';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
-  return <View />;
+  return <Text>Hello SignUp</Text>;
 }
