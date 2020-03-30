c26da2edbffd373dcd3ccb14b268e19c925a3a38
Léu Almeida <leo@webid.net.br>
2020-03-29 21:21:32 -0300

    chore(src/index): remove Routes import

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import { StatusBar } from 'react-native';
 
 // import { NavigationContainer } from '@react-navigation/native';
 
-// import Routes from './routes'
-
 export default function App() {
   return (
     <>
