c0ec4bddceab671849d04319ee0d09ae7eea5bef
Léu Almeida <leo@webid.net.br>
2020-03-30 17:05:24 -0300

    chore(routes): improving initial routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
 
 import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -12,50 +11,13 @@ import SignUp from './pages/SignUp';
 Icon.loadFont();
 
 const Stack = createStackNavigator();
-const Tabs = createBottomTabNavigator();
+// const Tabs = createBottomTabNavigator();
 
-function NewStack() {
+export default function createRouter() {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerTransparent: true,
-        headerTintColor: '#FFF',
-        headerLeftContainerStyle: {
-          marginLeft: 20,
-        },
-      }}
-    />
-  );
-}
-
-export default function createRouter(isSigned = false) {
-  return !isSigned ? (
     <Stack.Navigator headerMode="none">
       <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="SignUp" component={SignUp} />
     </Stack.Navigator>
-  ) : (
-    <Tabs.Navigator
-      tabBarOptions={{
-        activeTintColor: '#FFF',
-        inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
-        style: {
-          backgroundColor: '#8d41a8',
-        },
-        keyboardHidesTabBar: true,
-      }}
-    >
-      <Tabs.Screen
-        name="New"
-        component={NewStack}
-        options={{
-          tabBarVisible: false,
-          tabBarLabel: 'Agendar',
-          tabBarIcon: ({ color }) => (
-            <Icon name="add-circle-outline" size={20} color={color} />
-          ),
-        }}
-      />
-    </Tabs.Navigator>
   );
 }
