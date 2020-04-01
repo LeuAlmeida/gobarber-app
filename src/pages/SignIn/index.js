6103cc881c4646876783a38e860d1bc6876beae7
Léu Almeida <leo@webid.net.br>
2020-04-01 10:45:53 -0300

    feat(SignIn): import background style

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Background from '~/components/Background';
+
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <Text>Hello SignIn</Text>;
+  return (
+    <Background>
+      <Text>Hello SignIn</Text>
+    </Background>
+  );
 }
