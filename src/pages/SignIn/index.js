c4182840e81d15fb38279598d48dff32b64533e7
Léu Almeida <leo@webid.net.br>
2020-04-01 11:04:41 -0300

    feat(SignIn): import Input component

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '~/components/Input';
 import Background from '~/components/Background';
 
 // import { Container } from './styles';
@@ -9,6 +10,12 @@ export default function SignIn() {
   return (
     <Background>
       <Text>Hello SignIn</Text>
+
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
     </Background>
   );
 }
