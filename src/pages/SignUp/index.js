2bd59e9b66c41b82ea12cf4f54cda47787f5d262
Léu Almeida <leo@webid.net.br>
2020-04-04 20:51:40 -0300

    feat(SignUp): improving signup structure

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
-export default function SignUp() {
+export default function SinUp({ navigation }) {
   return (
     <Background>
-      <Text>Hello SignUp</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome Completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Cadastrar</SubmitButton>
+
+          <SignLink onPress={() => navigation.navigate('SignUp')}>
+            <SignLinkText>Já possuo login</SignLinkText>
+          </SignLink>
+        </Form>
+      </Container>
     </Background>
   );
 }
