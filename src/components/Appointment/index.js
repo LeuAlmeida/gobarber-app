6a44ead991a4e8c4491b8eecb6ab51f49a8744bc
Léu Almeida <leo@webid.net.br>
2020-04-07 15:22:34 -0300

    feat(Appointment): update component structure

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
 export default function Appointment() {
-  return <Container />;
+  return (
+    <Container>
+      <Left>
+        <Avatar
+          source={{ uri: 'https://api.adorable.io/avatar/50/leonardo.png' }}
+        />
+
+        <Info>
+          <Name>Leonardo Almeida</Name>
+          <Time>em 3 horas</Time>
+        </Info>
+      </Left>
+      <TouchableOpacity>
+        <Icon name="event-busy" size={20} color="#F54C75" />
+      </TouchableOpacity>
+    </Container>
+  );
 }
