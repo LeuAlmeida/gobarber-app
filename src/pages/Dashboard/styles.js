45585f3d2506ec964dcc4b30c2a6b21f81b11a3f
Léu Almeida <leo@webid.net.br>
2020-04-07 15:16:43 -0300

    style(Dashboard): improving FlatList styles

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -11,3 +11,8 @@ export const Title = styled.Text`
   align-self: center;
   margin-top: 30px;
 `;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { padding: 30 },
+})``;
