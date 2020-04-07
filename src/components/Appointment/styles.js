1df1487aee43fda5957c230dfa5395d63c0d3475
Léu Almeida <leo@webid.net.br>
2020-04-07 15:22:44 -0300

    style(Appointment): create styled components

--- src/components/Appointment/styles.js
+++ src/components/Appointment/styles.js
@@ -1,3 +1,13 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View``;
+
+export const Left = styled.View``;
+
+export const Avatar = styled.Image``;
+
+export const Info = styled.View``;
+
+export const Name = styled.Text``;
+
+export const Time = styled.Text``;
