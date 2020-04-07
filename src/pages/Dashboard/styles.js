0dde7b8632bea570ae7a03dfb9d2bfab3f64335f
Léu Almeida <leo@webid.net.br>
2020-04-07 15:11:53 -0300

    style(Dashboard): improving Dashboard stylesheet

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  font-weight: bold;
+  align-self: center;
+  margin-top: 30px;
+`;
