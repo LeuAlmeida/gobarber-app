7fc08e2ea5d837a6c506628c8b70c56da63d29b1
Léu Almeida <leo@webid.net.br>
2020-04-07 15:17:16 -0300

    feat(Appointment): improving initial structure

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Appointment() {
-  return <View />;
+  return <Container />;
 }
