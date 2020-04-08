8b6e101ee497d78dc47776edb81fc4be66fd446a
Léu Almeida <leo@webid.net.br>
2020-04-08 16:43:29 -0300

    style(Profile): incrementing styled component with real styles

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,13 +1,37 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 
-export const Title = styled.Text``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`;
 
-export const Separator = styled.View``;
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  font-weight: bold;
+  align-self: center;
+  margin-top: 30px;
+`;
 
-export const Form = styled.View``;
+export const Separator = styled.View`
+  height: 1px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 20px 0 30px;
+`;
 
-export const FormInput = styled.View``;
+export const Form = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { padding: 30 },
+})`
+  align-self: stretch;
+`;
 
-export const SubmitButton = styled.View``;
+export const FormInput = styled(Input)`
+  margin-bottom: 10px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 5px;
+`;
