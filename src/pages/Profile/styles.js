b2c453093c43742ae4ef2ae4529aebcc2c2725c5
Léu Almeida <leo@webid.net.br>
2020-04-08 16:40:26 -0300

    style(Profile): create style structure

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,3 +1,13 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View``;
+
+export const Title = styled.Text``;
+
+export const Separator = styled.View``;
+
+export const Form = styled.View``;
+
+export const FormInput = styled.View``;
+
+export const SubmitButton = styled.View``;
